01280e3adefc07b97168da978e297b9f6134c4af
adeemzia480 <adeemzia889977@gmail.com>
2024-05-07 12:27:04 +0500

    TeachStar_React_App

---
+++ src/components/Footer.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Footer() {
+  return <footer>
+    <div>
+        <h1>TechStar</h1>
+        <p>&copy; 2024 Copyrigt, All Rights Reserved</p>
+    </div>
+
+    <div>
+        <h5>Follow Us</h5>
+        <div>
+            <a href='www.youtube.com' target={'blank'}>YouTube</a>
+            <a href='www.instagram.com' target={'blank'}>Instagram</a>
+            <a href='www.github.com' target={'blank'}>GitHub</a>
+        </div>
+    </div>
+  </footer>
+}
+
+export default Footer
